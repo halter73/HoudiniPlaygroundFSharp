062d005f6b19fab87dc2b8a7f9014b71eaab684d
Stephen Halter <halter73@gmail.com>
2021-03-27 03:13:50 -0700

    use lambdas

--- Program.fs
+++ Program.fs
@@ -5,16 +5,12 @@ open Microsoft.AspNetCore.Mvc
 
 type Todo = { Id: int; Name: string; IsComplete: bool }
 
-let echoTodo ([<FromBody>] todo) = todo
-
-let getTodo () = { Id = 0; Name = "Play more!"; IsComplete = false }
-
 [<EntryPoint>]
 let main args =
     let app = WebApplication.Create(args)
 
-    app.MapPost("/", Func<Todo,Todo>(echoTodo)) |> ignore
-    app.MapGet("/", Func<Todo>(getTodo)) |> ignore
+    app.MapPost("/", Func<Todo,Todo>(fun todo -> todo)) |> ignore
+    app.MapGet("/", Func<Todo>(fun () -> { Id = 0; Name = "Play more!"; IsComplete = false })) |> ignore
 
     let task = app.RunAsync() |> Async.AwaitTask
     Async.RunSynchronously(task)
